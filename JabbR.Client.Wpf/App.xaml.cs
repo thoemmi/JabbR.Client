9587627eb264595c030906c12a8756bb95ad8e39
Thomas Freudenberg <info@thomasfreudenberg.com>
2012-06-08 09:07:48 +0200

    switched to Caliburn

--- JabbR.Client.Wpf/App.xaml.cs
+++ JabbR.Client.Wpf/App.xaml.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Windows;
+﻿using System.Windows;
 
 namespace JabbR.Client.Wpf {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application {
+        public App() {
+            InitializeComponent();
+        }
     }
 }
