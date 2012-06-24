5e5c4b8b4466c45a543167bd038ff2164e575581
Thomas Freudenberg <info@thomasfreudenberg.com>
2012-06-24 21:17:11 +0200

    clean-up

--- JabbR.Client.Wpf/AppBootstrapper.cs
+++ JabbR.Client.Wpf/AppBootstrapper.cs
@@ -5,13 +5,8 @@ using JabbR.Client.Wpf.Shell;
 namespace JabbR.Client.Wpf {
     public class AppBootstrapper : AutofacBootstrapper<ShellViewModel> {
         protected override void ConfigureBootstrapper() {
-            //  you must call the base version first!
             base.ConfigureBootstrapper();
-            //  override namespace naming convention
-            EnforceNamespaceConvention = false;
-            //  auto subsubscribe event aggregators
             AutoSubscribeEventAggegatorHandlers = true;
-
             ViewModelBaseType = typeof(IShell);
         }
 
